04a720ed18ba2bb7ab100e518fd2c8c7c49ca01b
Ana Kanashiro <bikoressawa@gmail.com>
2024-12-23 10:51:50 -0300

    install e implementação de cors

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const rotaLivro = require("./rotas/livro")
+const rotaLivro = require("./rotas/livro");
+const cors = require("cors");
 
 const app = express();
-
-app.use(express.json())
-
-app.use('/livros', rotaLivro)
+app.use(express.json());
+app.use(cors({ origin: "*" }));
+app.use("/livros", rotaLivro);
 
 const port = 8000;
 
 app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`)
-})
+  console.log(`Escutando a porta ${port}`);
+});
